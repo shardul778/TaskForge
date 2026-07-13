416499707f82e41c8c2f20e5cf69a61e545d5128
shardul778 <kadamshardul39@gmail.com>
2026-07-13 23:22:08 +0530

    Fix CORS

--- app/main.py
+++ app/main.py
@@ -16,10 +16,10 @@ app = FastAPI(title="TaskForge API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://127.0.0.1:5173",
-    "http://localhost:5173",
-    "https://task-forge-ashy-mu.vercel.app",
-],
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://task-forge-ashy-mu.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
